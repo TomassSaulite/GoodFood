89416d27bd1d8db1af0671b228be04bbb3237a7e
Tomass <tomassaulite@gmail.com>
2024-06-26 21:15:20 +0300

    small fixes

--- GoodFood/settings.py
+++ GoodFood/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'deliver.urls'
+ROOT_URLCONF = 'GoodFood.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'deliver.wsgi.application'
+WSGI_APPLICATION = 'GoodFood.wsgi.application'
 
 
 # Database
